d6c6b3c6997371060c2d1bbdc02f7075513d0ed7
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-26 13:17:36 +0330

    Use email-or-username authentication

--- config/settings.py
+++ config/settings.py
@@ -92,6 +92,7 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = Path(os.getenv("MEDIA_ROOT", BASE_DIR / "media"))
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+AUTHENTICATION_BACKENDS = ["apps.core.auth.EmailOrUsernameBackend"]
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "dashboard"
 LOGOUT_REDIRECT_URL = "login"

a9ac374c80d8255bd7700a3082829b5d6554f946
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-27 20:05:55 +0330

    Load compliance confirmation signals

--- apps/compliance/apps.py
+++ apps/compliance/apps.py
@@ -5,3 +5,6 @@ class ComplianceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.compliance"
     verbose_name = "Store compliance"
+
+    def ready(self):
+        from . import signals  # noqa: F401

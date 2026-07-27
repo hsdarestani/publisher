adca05453f49e61a379853b1a9acd30573f63ee5
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-27 13:11:02 +0330

    Register queued-job cloud wake signals

--- apps/publisher/apps.py
+++ apps/publisher/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
+
 class PublisherConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.publisher"
     verbose_name = "Publisher"
+
+    def ready(self):
+        from . import signals  # noqa: F401

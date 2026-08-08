41351604d79701d80e8cf2201c99c42cb2d48f83
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-08 20:08:42 +0330

    Merge Publisher release migration branches

---
+++ apps/publisher/migrations/0014_merge_a_esthetic_apple_and_aplus_solution.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('publisher', '0011_link_a_esthetic_apple_and_queue_ios'),
+        ('publisher', '0013_retry_aplus_solution_android_after_gradle_fix'),
+    ]
+
+    operations = []

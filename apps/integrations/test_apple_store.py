70b8fea89cb140e4f56947328633cdafeaf04ef6
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-09 11:46:38 +0330

    Test Apple first-version metadata retry

--- apps/integrations/test_apple_store.py
+++ apps/integrations/test_apple_store.py
@@ -1,9 +1,11 @@
 import json
+from types import SimpleNamespace
 from unittest.mock import Mock
 
 from django.test import SimpleTestCase
 
 from .apple_store import AppleStoreClient
+from .base import IntegrationError
 
 
 class AppleStoreVersionAlignmentTests(SimpleTestCase):
@@ -99,3 +101,44 @@ class AppleStoreVersionAlignmentTests(SimpleTestCase):
         result = client.ensure_version("app-1", "1.0.0")
 
         self.assertEqual(result, created)
+
+    def test_localization_retries_without_whats_new_when_apple_state_blocks_it(self):
+        client = self._client()
+        app = SimpleNamespace(
+            marketing_url="https://example.com",
+            support_url="https://example.com/support",
+        )
+        loc = SimpleNamespace(
+            locale="de-DE",
+            full_description="Beschreibung",
+            keywords="studio,ki",
+            promotional_text="Erstellen Sie Ihre App.",
+            release_notes="Erste Version",
+            app=app,
+        )
+        calls = []
+
+        def request(method, path, **kwargs):
+            calls.append((method, path, kwargs))
+            if method == "GET":
+                return {"data": [{"id": "loc-1"}]}
+            body = json.loads(kwargs["data"])
+            attrs = body["data"]["attributes"]
+            if "whatsNew" in attrs:
+                raise IntegrationError(
+                    "Apple API 409: Attribute 'whatsNew' cannot be edited at this time"
+                )
+            return {"data": {"id": "loc-1", "attributes": attrs}}
+
+        client.request = Mock(side_effect=request)
+
+        result = client.set_localization("version-1", loc)
+
+        self.assertEqual(result["id"], "loc-1")
+        patch_calls = [call for call in calls if call[0] == "PATCH"]
+        self.assertEqual(len(patch_calls), 2)
+        first_attrs = json.loads(patch_calls[0][2]["data"])["data"]["attributes"]
+        retry_attrs = json.loads(patch_calls[1][2]["data"])["data"]["attributes"]
+        self.assertEqual(first_attrs["whatsNew"], "Erste Version")
+        self.assertNotIn("whatsNew", retry_attrs)
+        self.assertEqual(retry_attrs["description"], "Beschreibung")
